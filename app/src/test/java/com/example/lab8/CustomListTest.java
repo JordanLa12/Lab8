163275d488530d8526405f35c0c223efc794f4e2
Jordan <jpla@ualberta.ca>
2023-03-10 14:19:04 -0700

    Fix deleteTest

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -4,6 +4,7 @@ package com.example.lab8;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 //import org.junit.Before;
@@ -11,6 +12,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 public class CustomListTest {
     private CustomList list;
@@ -57,18 +59,22 @@ public class CustomListTest {
      * remove 1 city
      * check if there are now only one city in the list
      * check if the city removed in not in the list
+     * checks if assertion is raised when no cities are in the list
      */
 
     @Test
-    public void deleteTest(City city){
+    public void deleteTest(){
         list = MockCityList();
-        City city1 = new City("Calgary", "AB");
-        list.addCity(city);
-        City city2 = new City("Calgary", "AB");
-        list.addCity(city);
+        City city1 = new City("Vancouver", "BC");
+        list.addCity(city1);
+        City city2 = new City("Toronto", "ON");
+        list.addCity(city2);
         assertEquals(list.getCount(), 2);
         list.delete(city1);
         assertEquals(list.getCount(), 1);
         assertFalse(list.hasCity(city1));
+        list.delete(city2);
+        assertThrows( NoSuchElementException.class, () -> {
+            list.delete(city1); });
     }
 }
